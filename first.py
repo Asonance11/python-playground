10b98b8cd4f493e93c607e5d45e9c2af02055776
Oluwadabira <adigunsamuelaso@gmail.com>
2023-09-28 14:33:54 +0100

    Working with variables

---
+++ first.py
@@ -0,0 +1,17 @@
+# Practice with variables
+
+first_name = "Adigun"
+last_name = "Samuel"
+full_name = first_name + " " + last_name
+print("Hello " + full_name)
+
+age = 19
+
+print('Your age is ' + str(age))
+price = 250.5
+print(price)
+print(type(price))
+
+human = False
+print(human)
+print(type(human))
