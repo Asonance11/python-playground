7fa5b52e11ea5ca58a49d2dcd3ea2284d5dfd97c
Oluwadabira <adigunsamuelaso@gmail.com>
2023-09-30 13:09:49 +0100

    Working with input

--- second.py
+++ second.py
@@ -8,3 +8,12 @@ y = int(y)
 print(x)
 print(y)
 print(z)
+
+# User input
+name = input("What is your name?: ")
+age = int(input("How old is your age?: "))
+height = float(input("How tall are you?: "))
+
+print("Your name is " + name)
+print("You are " + str(age) + " years old")
+print("You are " + str(height) + "cm tall")
