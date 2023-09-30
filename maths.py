2c5ae611f2e18555494f725e49ef0d79e489dd70
Oluwadabira <adigunsamuelaso@gmail.com>
2023-09-30 13:16:45 +0100

    math functions

---
+++ maths.py
@@ -0,0 +1,14 @@
+import math
+
+PI = 3.142
+x = 1 
+y = 2
+z = 3
+print(round(PI))
+print(math.ceil(PI))
+print(math.floor(PI))
+print(math.pow(PI, 2))
+print(abs(PI))
+print(math.sqrt(PI))
+print(min(x, y, z))
+print(max(x, y, z))
