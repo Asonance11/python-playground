71b486c1ab27e67d1aecad5de81ca601973aed39
Oluwadabira <adigunsamuelaso@gmail.com>
2023-10-07 12:57:11 +0100

    2d  lists

---
+++ 2dlists.py
@@ -0,0 +1,10 @@
+#  2D lists
+
+breakfast = ['eggs', 'akara', 'waffles']
+lunch = ['jollof', 'yam', 'beans']
+dinner = ['amala', 'eba', 'poundo']
+
+food = [breakfast, lunch, dinner]
+
+print(food)
+print(food[1][0])
