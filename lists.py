39513a7aba252810ebb7c513cce55cae78489b49
Oluwadabira <adigunsamuelaso@gmail.com>
2023-10-07 12:47:41 +0100

    Working with lists

---
+++ lists.py
@@ -0,0 +1,16 @@
+# lists
+
+food = ['amala', 'eba', 'rice', 'tofu', 'spaghetti']
+print(food)
+print(food[0])
+
+# list functions
+food.append('chicken')
+food.remove('eba')
+food.pop()
+food.insert(0, 'fish')
+food.sort()
+food.clear()
+
+for i in food:
+    print(i)
