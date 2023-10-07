cf1cb161881bfc815721489ac62e1a6b99c0b676
Oluwadabira <adigunsamuelaso@gmail.com>
2023-10-07 13:05:07 +0100

    tuples

---
+++ tuples.py
@@ -0,0 +1,12 @@
+# Tuples
+
+student = ('Me', 19, 'Male')
+
+print(student.count('Me'))
+print(student.index('Male'))
+
+for i in student:
+    print(i)
+
+if 'Me' in student:
+    print('I was here')
